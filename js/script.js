ffbc2c3fdc2a9d5231884ca5af92123d28d40f74
AlessandroVe <aveneziano536@gmail.com>
2021-06-24 15:27:12 +0200

    .

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
             data:{
                 text:'',
                 color:'',
+                indirizzo:"",
             },
 
         }
